cf4eac75cdfe37c1fa915c217d7c6298042a8ee7
Karan Choudhary <46473542+ncukaran18csu103@users.noreply.github.com>
2021-11-22 11:54:12 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    for(int row=0;row<=n-1;row++)
+    {
+        for(int col=0;col<=n-1;col++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<endl;
+    
+    for(int row=0;row<=n-1;row++)
+    {
+        for(int col=0;col<=row;col++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<endl;
+    for(int row=0;row<=n-1;row++)
+    {
+        for(int col=n-1;col>=row;col--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<endl;
+    for(int row=0;row<=n-1;row++)
+    {
+        for(int col=n;col>n-row;col--)
+        {
+            cout<<" ";
+        }
+        for(int col=0;col<n-row;col++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int row=0;row<=n-1;row++)
+    {
+        for(int col=0;col<n-row-1;col++)
+        {
+            cout<<" ";
+        }
+        for(int col=n-row;col<=n;col++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    cout<<endl;
+    /*
+        *
+       **
+      ***
+     ****
+    
+    */
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int i1=1;i1<=n;i1++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        for(int i1=1;i1<=i;i1++)
+        {
+            cout<<i1;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        for(int i1=i;i1<=n;i1++)
+        {
+            cout<<i1;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int i1=i;i1<=n;i1++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    
+}
